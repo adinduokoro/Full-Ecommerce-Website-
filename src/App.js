29dfb313b996b284d12443cdbd8acf31edd5e936
Addie Okoro <adinduokoro95@gmail.com>
2023-05-12 15:47:03 -0400

    Navbar
    The navbar structure and styling is created. Working on the slider below the Navbar next.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Main from "./Components/Main/Main"
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Main />
     </div>
-  );
+  ); 
 }
 
 export default App;
